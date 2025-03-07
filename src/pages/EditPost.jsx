b70a0cd6913627388f69dabc138968bb5931dd24
Siddhi Gupta <siddhigupta811@gmail.com>
2025-03-07 22:40:26 +0530

    Update EditPost.jsx

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { Container, PostForm } from "../Components";
+import { Container, PostForm } from "../components";
 import appwriteService from "../appwrite/config";
 
 function EditPost() {
